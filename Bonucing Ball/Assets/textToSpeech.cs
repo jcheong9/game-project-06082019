9936d7043e6fa60c00ac41c19cce75234737fae7
Smallyellowface <jctheathlete@gmail.com>
2020-01-05 13:50:32 -0800

    update audio button
    added audio

---
+++ Bonucing Ball/Assets/textToSpeech.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textToSpeech : MonoBehaviour
+{
+    public AudioSource _audio;
+    public InputField inputText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator DownloadTheAudio()
+    {
+        string url = "https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=SampleText&tl=En-gb";
+        WWW www = new WWW(url);
+
+        yield return www;
+
+        _audio.clip = www.GetAudioClip(false, true, AudioType.MPEG);
+        _audio.Play();
+    }
+
+    public void ButtonPlayAudio()
+    {
+        StartCoroutine(DownloadTheAudio());
+    }
+
+}

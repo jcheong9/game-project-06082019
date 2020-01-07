6b0b5843bef2cbbfa0b18e87541f156804104e2c
Smallyellowface <jctheathlete@gmail.com>
2020-01-06 19:19:46 -0800

    commit

--- Bonucing Ball/Assets/RestClient/DemoScene/Scripts/MainScript.cs
+++ Bonucing Ball/Assets/RestClient/DemoScene/Scripts/MainScript.cs
@@ -6,78 +6,78 @@ using System.Collections.Generic;
 
 public class MainScript : MonoBehaviour {
 
-    private readonly string basePath = "https://wordsapiv1.p.mashape.com/words";
-	private RequestHelper currentRequest;
+ //   private readonly string basePath = "https://wordsapiv1.p.mashape.com/words";
+	//private RequestHelper currentRequest;
 
-	public void Get(){
+	//public void Get(){
 
-		// We can add default request headers for all requests
-		RestClient.DefaultRequestHeaders["Authorization"] = "Bearer ...";
+	//	// We can add default request headers for all requests
+	//	RestClient.DefaultRequestHeaders["Authorization"] = "Bearer ...";
 
-        RequestHelper requestOptions = null;
+ //       RequestHelper requestOptions = null;
 
-		RestClient.GetArray<Post>(basePath + "/example").Then(res => {
-            EditorUtility.DisplayDialog ("Posts", JsonHelper.ArrayToJsonString<Post>(res, true), "Ok");
-            return RestClient.GetArray<Todo>(basePath + "/todos");
-		}).Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
-	}
+	//	RestClient.GetArray<Post>(basePath + "/example").Then(res => {
+ //           EditorUtility.DisplayDialog ("Posts", JsonHelper.ArrayToJsonString<Post>(res, true), "Ok");
+ //           return RestClient.GetArray<Todo>(basePath + "/todos");
+	//	}).Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
+	//}
 
-	public void Post(){
+	//public void Post(){
 
-		currentRequest = new RequestHelper {
-			Uri = basePath + "/posts",
-			Body = new Post {
-				title = "foo",
-				body = "bar",
-				userId = 1
-			}
-		};
-		RestClient.Post<Post>(currentRequest)
-		.Then(res => EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(res, true), "Ok"))
-		.Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
-	}
+	//	currentRequest = new RequestHelper {
+	//		Uri = basePath + "/posts",
+	//		Body = new Post {
+	//			title = "foo",
+	//			body = "bar",
+	//			userId = 1
+	//		}
+	//	};
+	//	RestClient.Post<Post>(currentRequest)
+	//	.Then(res => EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(res, true), "Ok"))
+	//	.Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
+	//}
 
-	public void Put(){
+	//public void Put(){
 
-		currentRequest = new RequestHelper {
-			Uri = basePath + "/posts/1", 
-			Body = new Post {
-				title = "foo",
-				body = "bar",
-				userId = 1
-			},
-			Retries = 5,
-			RetrySecondsDelay = 1,
-			RetryCallback = (err, retries) => {
-				Debug.Log (string.Format ("Retry #{0} Status {1}\nError: {2}", retries, err.StatusCode, err));
-			}
-		};
-		RestClient.Put<Post>(currentRequest, (err, res, body) => {
-			if(err != null){
-				EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
-			}
-			else{
-				EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(body, true), "Ok");
-			}
-		});
-	}
+	//	currentRequest = new RequestHelper {
+	//		Uri = basePath + "/posts/1", 
+	//		Body = new Post {
+	//			title = "foo",
+	//			body = "bar",
+	//			userId = 1
+	//		},
+	//		Retries = 5,
+	//		RetrySecondsDelay = 1,
+	//		RetryCallback = (err, retries) => {
+	//			Debug.Log (string.Format ("Retry #{0} Status {1}\nError: {2}", retries, err.StatusCode, err));
+	//		}
+	//	};
+	//	RestClient.Put<Post>(currentRequest, (err, res, body) => {
+	//		if(err != null){
+	//			EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
+	//		}
+	//		else{
+	//			EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(body, true), "Ok");
+	//		}
+	//	});
+	//}
 
-	public void Delete(){
+	//public void Delete(){
 
-		RestClient.Delete(basePath + "/posts/1", (err, res) => {
-			if(err != null){
-				EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
-			}
-			else{
-				EditorUtility.DisplayDialog ("Success", "Status: " + res.StatusCode.ToString(), "Ok");
-			}
-		});
-	}
+	//	RestClient.Delete(basePath + "/posts/1", (err, res) => {
+	//		if(err != null){
+	//			EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
+	//		}
+	//		else{
+	//			EditorUtility.DisplayDialog ("Success", "Status: " + res.StatusCode.ToString(), "Ok");
+	//		}
+	//	});
+	//}
 
-	public void AbortRequest(){
-		if (currentRequest != null) {
-			currentRequest.Abort();
-			currentRequest = null;
-		}
-	}
+	//public void AbortRequest(){
+	//	if (currentRequest != null) {
+	//		currentRequest.Abort();
+	//		currentRequest = null;
+	//	}
+	//}
 }
